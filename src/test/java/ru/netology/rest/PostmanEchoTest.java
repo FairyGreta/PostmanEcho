6ca4bc0d282c1454f7a684ad199a1844b247a6da
FairyGreta <Christina_Art@mail.ru>
2022-08-12 19:36:39 +0500

    Роняем тест shouldSendRequestPostTest

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -24,7 +24,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Test"));
+                .body("data", equalTo("Тест"));
     }
 
     //пробуем вернуть POST-запрос с объектом
